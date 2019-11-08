dbebc2466bf71829ed907b2ecd691b77aa55751b
mishel-s <s_mishel_s@ukr.net>
2019-11-08 16:49:14 +0200

    Updates

---
+++ precache-manifest.7faca6171a3c83e2cef71c283064bf4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9880be797b1ee7be18cb251ee8a2e788",
+    "url": "/chartsjs/index.html"
+  },
+  {
+    "revision": "ea2dd1157bd3744b3064",
+    "url": "/chartsjs/static/css/main.e59289c3.chunk.css"
+  },
+  {
+    "revision": "3409b261abbe7ca36e71",
+    "url": "/chartsjs/static/js/2.f3a31175.chunk.js"
+  },
+  {
+    "revision": "ea2dd1157bd3744b3064",
+    "url": "/chartsjs/static/js/main.01f46e8e.chunk.js"
+  },
+  {
+    "revision": "2035abaca20c8a1219a1",
+    "url": "/chartsjs/static/js/runtime~main.d1f1052a.js"
+  }
+]);
